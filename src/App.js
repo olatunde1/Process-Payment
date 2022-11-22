d11af5353e60b3a0da6ab70d4b89695b20faf1da
Adetunji <idowuolatunde32@gmail.com>
2022-11-23 00:43:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { Button } from "./components/ButtonStyle";
 
-function App() {
+import { useState } from "react";
+import { useFlutterwave, closePaymentModal } from "flutterwave-react-v3";
+
+export default function App() {
+  const [amount, setAmount] = useState(0);
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const config = {
+    public_key: "FLWPUBK_TEST-e7c8f332b9d34b01b958cf4f4f643018-X",
+    tx_ref: Date.now(),
+    amount: amount,
+    currency: "NGN",
+    payment_options: "card,mobilemoney,ussd",
+    customer: {
+      email: email,
+      phone_number: phone,
+      name: name,
+    },
+    customizations: {
+      title: "my Payment Title",
+      description: "Payment for items in cart",
+      logo: "https://st2.depositphotos.com/4403291/7418/v/450/depositphotos_74189661-stock-illustration-online-shop-log.jpg",
+    },
+  };
+
+  const handleFlutterPayment = useFlutterwave(config);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="container">
+        <input
+          type="number"
+          placeholder="Amount:"
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Phone"
+          onChange={(e) => setPhone(e.target.value)}
+        />
+
+        <Button
+          onClick={() =>
+            handleFlutterPayment({
+              callback: (response) => {
+                console.log(response);
+                closePaymentModal();
+              },
+              onClose: () => {},
+            })
+          }
         >
-          Learn React
-        </a>
-      </header>
+          Pay
+        </Button>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/ButtonStyle.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+
+export const Button = styled.button`
+    width: 100%;
+    max-width: 400px;
+    padding: 1rem;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    outline: none;
+    font-size: 1.2rem;
+    margin-bottom: 1rem;
+    background: skyblue;
+    cursor: pointer;
+    color: #fff;
+
+    &:hover{
+        background: deepskyblue;
+    }
+`
